d36f74c3d6ba2c30a93bff1ea70dc75ef81791f4
OhDricky <97364462+OhDricky@users.noreply.github.com>
2022-01-09 10:54:31 -0600

    Fixed en_us.lang and endergon ore
    Revised code for endergone ore and added endergon ore to en_us.lang

--- src/main/java/me/sandbox/block/ModBlocks.java
+++ src/main/java/me/sandbox/block/ModBlocks.java
@@ -8,9 +8,7 @@ import net.minecraft.block.material.Material;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
-import net.minecraft.item.PickaxeItem;
 import net.minecraftforge.common.ToolType;
-import net.minecraftforge.eventbus.EventBus;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.RegistryObject;
 import net.minecraftforge.registries.DeferredRegister;
@@ -20,29 +18,26 @@ import java.util.function.Supplier;
 
 public class ModBlocks {
 
-public static final DeferredRegister<Block> BLOCKS
-        = DeferredRegister.create(ForgeRegistries.BLOCKS, Sandbox.MOD_ID);
+    public static final DeferredRegister<Block> BLOCKS
+            = DeferredRegister.create(ForgeRegistries.BLOCKS, Sandbox.MOD_ID);
 
-public static final RegistryObject<Block> ENDERGON_ORE =registerBlock("endergon_ore",
-        () -> new Block(AbstractBlock.Properties.create(Material.ROCK).harvestLevel(4).harvestTool(ToolType.PICKAXE).setRequiresTool().hardnessAndResistance(8f)));
-
-
-private static <T extends Block> RegistryObject<T> registerBlock(String name, Supplier<T> block) {
-    RegistryObject<T> toReturn = BLOCKS.register(name, block);
-    return toReturn;
-
-}
-
-private static <T extends Block> void registerBlockItem(String name, RegistryObject<T> block) {
-    ModItems.ITEMS.register(name, () -> new BlockItem(block.get(),
-            new Item.Properties().group(ItemGroup.BUILDING_BLOCKS)));
-}
-public static void register(IEventBus eventBus) {
-    BLOCKS.register(eventBus);
-}
+    public static final RegistryObject<Block> ENDERGON_ORE = registerBlock("endergon_ore",
+            () -> new Block(AbstractBlock.Properties.create(Material.ROCK)
+                    .harvestLevel(4).setRequiresTool().harvestTool(ToolType.PICKAXE).hardnessAndResistance(8f)));
 
 
+    private static <T extends Block>RegistryObject<T> registerBlock(String name, Supplier<T> block) {
+        RegistryObject<T> toReturn = BLOCKS.register(name, block);
+        registerBlockItem(name, toReturn);
+        return toReturn;
+    }
 
+    private static <T extends Block> void registerBlockItem(String name, RegistryObject<T> block) {
+        ModItems.ITEMS.register(name, () -> new BlockItem(block.get(),
+                new Item.Properties().group(ItemGroup.BUILDING_BLOCKS)));
+    }
 
-
+    public static void register(IEventBus eventBus) {
+        BLOCKS.register(eventBus);
+    }
 }
