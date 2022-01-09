18ef4085fa927970750534612b529d3e74fa6fc2
OhDricky <97364462+OhDricky@users.noreply.github.com>
2022-01-08 23:46:57 -0600

    Coded in enderite armor
    Coded in enderite armor, needs textures!

--- src/main/java/me/sandbox/item/ModItems.java
+++ src/main/java/me/sandbox/item/ModItems.java
@@ -1,6 +1,8 @@
 package me.sandbox.item;
 
 import me.sandbox.Sandbox;
+import net.minecraft.inventory.EquipmentSlotType;
+import net.minecraft.item.ArmorItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
 import net.minecraftforge.eventbus.api.IEventBus;
@@ -13,8 +15,18 @@ public class ModItems {
     public static final DeferredRegister<Item> ITEMS =
             DeferredRegister.create(ForgeRegistries.ITEMS, Sandbox.MOD_ID);
 
+    //Register Items here
+
     public static final RegistryObject<Item> ENDERITE_INGOT = ITEMS.register("enderite_ingot",
             () -> new Item(new Item.Properties().group(ItemGroup.MATERIALS)));
+    public static final RegistryObject<Item> ENDERITE_HELMET = ITEMS.register("enderite_helmet", () ->
+            new ArmorItem(ModArmorMaterial.ENDERITE, EquipmentSlotType.HEAD, new Item.Properties().group(ItemGroup.COMBAT)));
+    public static final RegistryObject<Item> ENDERITE_CHESTPLATE = ITEMS.register("enderite_chestplate", () ->
+            new ArmorItem(ModArmorMaterial.ENDERITE, EquipmentSlotType.CHEST, new Item.Properties().group(ItemGroup.COMBAT)));
+    public static final RegistryObject<Item> ENDERITE_LEGGINGS = ITEMS.register("enderite_leggings", () ->
+            new ArmorItem(ModArmorMaterial.ENDERITE, EquipmentSlotType.LEGS, new Item.Properties().group(ItemGroup.COMBAT)));
+    public static final RegistryObject<Item> ENDERITE_BOOTS = ITEMS.register("enderite_boots", () ->
+            new ArmorItem(ModArmorMaterial.ENDERITE, EquipmentSlotType.FEET, new Item.Properties().group(ItemGroup.COMBAT)));
 
 
 
