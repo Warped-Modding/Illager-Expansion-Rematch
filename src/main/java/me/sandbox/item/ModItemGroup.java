97ebe0f1e384d68c63b0c4674c19f0e6301c027a
Kalion2306 <97402015+Kalion2306@users.noreply.github.com>
2022-02-21 19:52:46 +0000

    Added Decoration Item Group

--- src/main/java/me/sandbox/item/ModItemGroup.java
+++ src/main/java/me/sandbox/item/ModItemGroup.java
@@ -1,12 +1,16 @@
 package me.sandbox.item;
 
 import me.sandbox.Sandbox;
+import me.sandbox.block.BlockRegistry;
 import net.fabricmc.fabric.api.client.itemgroup.FabricItemGroupBuilder;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.Identifier;
 
 public class ModItemGroup {
+
+    public static final ItemGroup SandBoxDecorations = FabricItemGroupBuilder.build(new Identifier(Sandbox.MOD_ID, "sandboxdecor"),
+            () -> new ItemStack(BlockRegistry.endergon_ore));
 
     public static final ItemGroup SandBoxMisc = FabricItemGroupBuilder.build(new Identifier(Sandbox.MOD_ID, "sandboxmisc"),
             () -> new ItemStack(ItemRegistry.ENDERGON_INGOT));
