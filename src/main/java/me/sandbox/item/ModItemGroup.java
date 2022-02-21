6bf5e073bcda4a8a459d2b071a2b71925fb9701b
Kalion2306 <97402015+Kalion2306@users.noreply.github.com>
2022-02-21 18:23:18 +0000

    Added in Creative Tabs
    Added in two creative tabs, one for Mobs and the other for everything else.

---
+++ src/main/java/me/sandbox/item/ModItemGroup.java
@@ -0,0 +1,16 @@
+package me.sandbox.item;
+
+import me.sandbox.Sandbox;
+import net.fabricmc.fabric.api.client.itemgroup.FabricItemGroupBuilder;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.Identifier;
+
+public class ModItemGroup {
+
+    public static final ItemGroup SandBoxMisc = FabricItemGroupBuilder.build(new Identifier(Sandbox.MOD_ID, "sandboxmisc"),
+            () -> new ItemStack(ItemRegistry.ENDERGON_INGOT));
+
+    public static final ItemGroup SandBoxMobs = FabricItemGroupBuilder.build(new Identifier(Sandbox.MOD_ID, "sandboxmobs"),
+            () -> new ItemStack(ItemRegistry.ENDERGON_INGOT));
+}
